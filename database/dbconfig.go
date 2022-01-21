b71cf373df1082b889001589f0d70e05f8d4c91e
Pedro Rodrigues <pedro.rodrigues@thoughtworks.com>
2022-01-20 21:39:47 -0300

    Add connection to Sqlite for testing

--- database/dbconfig.go
+++ database/dbconfig.go
@@ -1,16 +1,17 @@
 package dbconfig
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 
 	"github.com/pedrovitorlima/stock-wallet-backend/models"
 	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 const PostgresDriver = "postgres"
+const SqlLiteDriver = "sqllite"
 const User = "pgtest"
 const Host = "localhost"
 
@@ -20,14 +21,22 @@ const Password = "1234"
 
 const DbName = "stock-wallet-db"
 
-var DataSourceName = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", Host, Port, User, Password, DbName)
-
-func GetConnection() (*sql.DB, error) {
-	return sql.Open(PostgresDriver, DataSourceName)
-}
+var DataSourceNamePostgres = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", Host, Port, User, Password, DbName)
 
 func Init() *gorm.DB {
-	db, err := gorm.Open(postgres.Open(DataSourceName), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(DataSourceNamePostgres), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(&models.Wallet{})
+
+	return db
+}
+
+func InitSqlLiteForTest() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err)
--- go.mod
+++ go.mod
@@ -18,10 +18,12 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/lib/pq v1.10.4 // indirect
+	github.com/mattn/go-sqlite3 v1.14.10 // indirect
 	github.com/steinfletcher/apitest v1.5.11 // indirect
 	github.com/steinfletcher/apitest-jsonpath v1.7.1 // indirect
 	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gorm.io/driver/postgres v1.2.3 // indirect
+	gorm.io/driver/sqlite v1.2.6 // indirect
 	gorm.io/gorm v1.22.5 // indirect
 )
